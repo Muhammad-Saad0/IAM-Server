b8ae149ec0eac4c26f2a49290e4c5973f721bd66
Muhammad Saad <muhammadsaad3498@gmail.com>
2026-06-05 01:46:19 +0500

    initial commit

---
+++ src/test/java/com/saad/iam/IamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saad.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
