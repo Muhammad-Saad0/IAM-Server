3af4a15f49ad7fc6a25392866bf7d3e0c188e6e6
Muhammad Saad <muhammadsaad3498@gmail.com>
2026-06-16 21:02:43 +0500

    chore: trigger redeploy

--- src/main/java/com/example/iam/account/adapter/in/bootstrap/AdminUserBootstrap.java
+++ src/main/java/com/example/iam/account/adapter/in/bootstrap/AdminUserBootstrap.java
@@ -12,7 +12,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.Clock;
 import java.time.Instant;
 
